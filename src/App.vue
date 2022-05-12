525e2a886ebbd47b64b3848a6f5ed9205b68c239
Jungjjeong <sky990106@dgu.ac.kr>
2022-05-12 21:42:01 +0900

    Create project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <Hello />
+</template>
+
+<script>
+import Hello from "~/components/Hello";
+
+export default {
+  components: {
+    Hello,
+  },
+  data() {
+    return {
+      msg: "Hello Vue!",
+    };
+  },
+};
+</script>
+
+
+<style>
+h1 {
+  color: tomato;
+}
+</style>
